cdb505f45cdf7b11b58c5c2620d27922fc3d589b
Shabid5 <87390087+Shabid5@users.noreply.github.com>
2022-02-02 15:19:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 5) {
+            $(".nav-bar").addClass("sticky");
+        } else {
+            $(".nav-bar").removeClass("sticky");
+        }
+
+        if (scrolling > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+
+    });
+
+    // Slide UP 
+    $(".scroll-up-btn").click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // TOGGLE 
+    $('.menu-btn').click(function () {
+        $('.nav-bar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+
+
+    // TYPED 
+    var typed = new Typed(".typing", {
+        strings: ["Youtuber", "Developer", "Designer", "Programmer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Youtuber", "Developer", "Designer", "Programmer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+
+    // owl carousel script 
+    $('.carousell').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 3,
+                nav: false,
+            }
+        }
+
+    });
+});
